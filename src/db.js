7db28ba1b0440697093fc80d171e84740c8e8732
ninetu <ninetu@gmail.com>
2017-06-24 00:20:42 +0700

    Add VueBootstrap

---
+++ src/db.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+var config = {
+  apiKey: 'AIzaSyCu5dsqmkcIjHAV9V4hWCAO9vy7IYYXw_s',
+  authDomain: 'pwa-online.firebaseapp.com',
+  databaseURL: 'https://pwa-online.firebaseio.com',
+  projectId: 'pwa-online',
+  storageBucket: 'pwa-online.appspot.com',
+  messagingSenderId: '199988461481'
+}
+const firebaseApp = firebase.initializeApp(config)
+const db = firebaseApp.database()
+export default db
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueFire from 'vuefire'
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(VueFire)
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
