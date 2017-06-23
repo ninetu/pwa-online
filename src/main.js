6f3d8ab1a25aac5178e2bae5b30649bb0134e576
ninetu <ninetu@gmail.com>
2017-06-24 01:33:26 +0700

    Add bootstrap css

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import BootstrapVue from 'bootstrap-vue'
 Vue.use(VueFire)
 Vue.use(BootstrapVue)
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
